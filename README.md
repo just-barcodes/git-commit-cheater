558700ad5cf3c1519ea251378e6042f2fe382d07
just-barcodes <justbarcodes@gmail.com>
2015-04-12 00:00:00 +0200

    cheating

---
+++ README.md
@@ -0,0 +1,10 @@
+# GIT COMMIT CHEATER
+Adds a useless commit to the last few days.
+
+### Usage
+```
+$ python3 cheat.py <num>
+```
+This will add a commit to the last <num> days and push it.
+You will have to have your remote and SSH key set up for this
+to work.
---
+++ cheat.py
@@ -0,0 +1,57 @@
+"""
+Github Commit Cheater
+2015
+"""
+
+import sys
+from datetime import date, timedelta
+import os
+
+
+def print_usage(scriptname):
+    print("USAGE:")
+    print("$ python3 " + scriptname + " <num_of_past_days>")
+    print()
+
+
+def cheat(num):
+    today = date.today()
+    for n in range(num):
+        cheatday = today - timedelta(days=n)
+        cheatday = str(cheatday) + " 0:0:0"
+        os.system(change_file(cheatday) +
+                  change_date(cheatday) +
+                  commit('cheating'))
+    os.system(push())
+
+
+def change_file(filecontent):
+    change_file = "echo '" + filecontent + "' > cheatfile.txt; "
+    add_files = "git add -A; "
+    return change_file + add_files
+
+
+def change_date(cheatday):
+    export_author_date = "export GIT_AUTHOR_DATE='" + cheatday + "'; "
+    export_commit_date = "export GIT_COMMITTER_DATE='" + cheatday + "'; "
+    return export_author_date + export_commit_date
+
+
+def commit(msg):
+    return "git commit -m '" + msg + "'; "
+
+
+def push():
+    return "git push origin master; "
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print_usage(sys.argv[0])
+        sys.exit()
+    try:
+        cheat(int(sys.argv[1]))
+    except ValueError:
+        print("Wrong Argument!")
+        print_usage(sys.argv[0])
+        sys.exit()
